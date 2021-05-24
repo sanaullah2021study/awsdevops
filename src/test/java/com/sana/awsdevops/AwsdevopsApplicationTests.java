1e15f050ca92163bb004c4913b7f352bbd096f30
jdevsana <jdev.sana@gmail.com>
2021-05-24 14:12:43 +1000

    Initial commit

---
+++ src/test/java/com/sana/awsdevops/AwsdevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sana.awsdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsdevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
